f0569d80412358925d6f6b1a625060fbe24d9022
Marius Bär <marius.baer@proton.me>
2024-11-12 21:26:02 +0100

    Make servings input field match theme

--- static/js/recipe.js
+++ static/js/recipe.js
@@ -1,8 +1,11 @@
 
+
 document.addEventListener('DOMContentLoaded', function () {
     const servingsInput = document.getElementById('servings-input');
     const ingredientQuantities = document.querySelectorAll('.ingredient-quantity');
     const defaultServings = document.getElementById('recipe-ingredients').getAttribute('default-servings');
+    const minusButton = document.querySelector(".minus");
+    const plusButton = document.querySelector(".plus");
 
     function updateQuantities() {
         const servings = parseFloat(servingsInput.value);
@@ -11,6 +14,20 @@ document.addEventListener('DOMContentLoaded', function () {
             quantityElement.textContent = (baseQuantity * servings / defaultServings).toLocaleString('de-DE', { useGrouping: false });
         });
     }
-
     servingsInput.addEventListener('input', updateQuantities);
+
+    minusButton.addEventListener("click", () => {
+        if (--servingsInput.value <= 1) {
+            minusButton.disabled = true;
+        } ;
+        updateQuantities();
+    });
+
+    plusButton.addEventListener("click", () => {
+        if (++servingsInput.value > 1) {
+            minusButton.disabled = false;
+        }
+        updateQuantities();
+    });
 });
+
